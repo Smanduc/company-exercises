7c19188d146de5cb38604f20413758a2d2274135
BiancaSeleveschi <bianca.seleveschi@gmail.com>
2022-07-18 15:52:02 +0300

    add first project files

---
+++ script.js
@@ -0,0 +1,27 @@
+// //Declaram variabila pt elementele din html
+// let div = document.createElement("div")
+// did.id = "new-div"
+// let inputElement = document.getElementById("contact-input")
+// let buttonElement = document.getElementById("contact-button")
+// let listenElement = document.getElementById("contact-ul")
+
+// //identificam actiunea din pagina si atasam un event listener pe el (ascultam, dupa evenimente pe acel element, pe buton)
+// buttonElement.addEventListener("click", addEmail)
+
+// function increment(x) {
+//   return x;
+// }
+
+// function addEmail() {
+//   //extragem email ul din input
+//   let inputValue = inputElement.value
+//   console.log(inputValue);
+//   //adaugam o adresa de email 
+//   //cream un element de tip span unde sa fie adaugat email-ul
+//   var selectedEmail = document.createEmail()
+//   //setam valoarea din span cu cea din input
+//   newSpan.apprendChild(selectedEmail);
+//   // var.innerText = inputValue;
+//   // //atasam emai ul in lista lui
+//   // listenElement.apprendChild(i)
+// }
